e4d4f72438854b072e62f04d49f2d18db81da383
rajeev <rajivg216@gmail.com>
2025-02-16 22:02:47 +0530

    Inital commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [RouterOutlet],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  
+}
