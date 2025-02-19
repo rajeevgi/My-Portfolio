605a09e63f4d058fb9c2abbdd96f2ee153f43d5f
rajeev <rajivg216@gmail.com>
2025-02-19 21:59:13 +0530

    Contact Form Added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { FooterComponent } from '../../components/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
